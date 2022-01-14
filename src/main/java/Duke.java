01bdd47933446217d7da635344f9c95d12e285fe
vishandi <>
2022-01-14 23:17:20 +0800

    Each text inputted is stored to a list. Added "list" function.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,16 +2,27 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        String[] itemList = new String[100];
+        int numItem = 0;
         System.out.println("Hello! I'm Dr.Kafka!");
         System.out.println("What can I do for you?");
-        while(true) {
+        while (true) {
             String userInput = sc.nextLine();
-            if(userInput.equals("bye")) {
+            if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < numItem; i++) {
+                    System.out.print(i + 1);
+                    System.out.print(". ");
+                    System.out.println(itemList[i]);
+                }
+            } else {
+                itemList[numItem] = userInput;
+                numItem += 1;
+                System.out.print("added: ");
+                System.out.println(userInput);
             }
-            System.out.println(userInput);
         }
     }
 }
