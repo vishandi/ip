e7856fcc5189574a567f7073d96f82f5c54c4bb1
vishandi <didi@Vishandis-MacBook-Pro.local>
2022-01-14 22:43:20 +0800

    Added greet, echo, and exit (bye) function.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Dr.Kafka!");
+        System.out.println("What can I do for you?");
+        while(true) {
+            String userInput = sc.nextLine();
+            if(userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(userInput);
+        }
     }
 }
